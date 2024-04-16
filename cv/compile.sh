66124d4fce2406b9c686d64cafe246cc5c086422
Yingtao Tian <alan.yt.tian@gmail.com>
2024-04-16 12:23:43 +0900

    .

--- cv/compile.sh
+++ cv/compile.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
+set -u
+set -o pipefail
+
 function warning () {
   fg_color=1 # red
   echo "$(tput bold)$(tput setaf $fg_color)WARNING: $@$(tput sgr0)"
 }
 
-pdflatex cv.tex || warning "pdflatex failed!"
-rm -f cv.log
+function compile(){
+  FILENAME="${1:-cv}"
+  shift
 
-rm -f cv.html
-docker run -ti --rm -v $(pwd .):/pdf bwits/pdf2htmlex pdf2htmlEX --zoom 1.3 cv.pdf || warning "pdf2htmlEX failed!"
+  pdflatex "$FILENAME.tex" || warning "pdflatex failed!"
+  rm -f "$FILENAME.log"
+  rm -f "$FILENAME.html"
+  docker run -ti --rm -v $(pwd .):/pdf bwits/pdf2htmlex pdf2htmlEX --zoom 1.3 "$FILENAME.pdf" || warning "pdf2htmlEX failed!"
+}
+
+function main() {
+  compile "cv"
+  compile "cv2"
+}
+
+main
